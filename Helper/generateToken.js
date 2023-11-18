efd18e62c06fbbcfc99f95da80341a49a11cf446
Abdulwasim-S <abdulwasimsmech@gmail.com>
2023-11-19 00:37:34 +0530

    token-duration-increased

--- Helper/generateToken.js
+++ Helper/generateToken.js
@@ -3,13 +3,13 @@ import {} from "dotenv/config.js";
 
 export async function generateToken(value) {
   const token = jwt.sign({ id: value }, process.env.SECRET_KEY, {
-    expiresIn: "1h",
+    expiresIn: "1d",
   });
   return token;
 }
 export async function generateUserToken(value) {
   const token = jwt.sign({ id: value }, process.env.SECRET_KEY_USER, {
-    expiresIn: "1h",
+    expiresIn: "1d",
   });
   return token;
 }
